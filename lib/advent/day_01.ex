9ca7368cf8ea7bf78d861467bff98f28f5de0663
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2020-12-01 22:54:15 +0100

    Day 1, part 2

--- lib/advent/day_01.ex
+++ lib/advent/day_01.ex
@@ -24,6 +24,11 @@ defmodule Advent.Day01 do
   In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
 
   Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
+
+  --- Part Two ---
+  The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+
+  Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Mul
   """
 
   @doc """
@@ -37,6 +42,23 @@ defmodule Advent.Day01 do
     |> Stream.map(&{&1, 2020 - &1})
     |> Enum.find(fn {_a, b} -> b in numbers end)
     |> (fn {a, b} -> a * b end).()
+  end
+
+  @doc """
+  Part 2
+  """
+  @spec find_2020_sum_3(String.t()) :: integer
+  def find_2020_sum_3(input) do
+    numbers = parse(input)
+
+    for a <- numbers,
+        b <- numbers do
+      {a, b}
+    end
+    |> Stream.reject(fn {a, b} -> a == b end)
+    |> Stream.map(fn {a, b} -> {a, b, 2020 - a - b} end)
+    |> Enum.find(fn {_a, _b, c} -> c in numbers end)
+    |> (fn {a, b, c} -> a * b * c end).()
   end
 
   defp parse(input) do
--- test/advent/day_01_test.exs
+++ test/advent/day_01_test.exs
@@ -23,4 +23,25 @@ defmodule Advent.Day01Test do
              |> Day01.find_2020_sum() == 842_016
     end
   end
+
+  describe "part 2" do
+    test "example" do
+      input = """
+      1721
+      979
+      366
+      299
+      675
+      1456
+      """
+
+      assert Day01.find_2020_sum_3(input) == 241_861_950
+    end
+
+    test "puzzle input" do
+      assert "puzzle_inputs/day_01.txt"
+             |> File.read!()
+             |> Day01.find_2020_sum_3() == 9_199_664
+    end
+  end
 end
