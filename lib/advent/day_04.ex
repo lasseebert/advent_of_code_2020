f27e880c20d839bc74f0deb4e14dee44575a00e0
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2020-12-04 09:07:43 +0100

    Day 4, part 2

--- lib/advent/day_04.ex
+++ lib/advent/day_04.ex
@@ -43,6 +43,71 @@ defmodule Advent.Day04 do
   According to the above rules, your improved system would report 2 valid passports.
 
   Count the number of valid passports - those that have all required fields. Treat cid as optional. In your batch file, how many passports are valid?
+
+  --- Part Two ---
+  The line is moving more quickly now, but you overhear airport security talking about how passports with invalid data are getting through. Better add some data validation, quick!
+
+  You can continue to ignore the cid field, but each other field has strict rules about what values are valid for automatic validation:
+
+  byr (Birth Year) - four digits; at least 1920 and at most 2002.
+  iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+  eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+  hgt (Height) - a number followed by either cm or in:
+  If cm, the number must be at least 150 and at most 193.
+  If in, the number must be at least 59 and at most 76.
+  hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+  ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+  pid (Passport ID) - a nine-digit number, including leading zeroes.
+  cid (Country ID) - ignored, missing or not.
+  Your job is to count the passports where all required fields are both present and valid according to the above rules. Here are some example values:
+
+  byr valid:   2002
+  byr invalid: 2003
+
+  hgt valid:   60in
+  hgt valid:   190cm
+  hgt invalid: 190in
+  hgt invalid: 190
+
+  hcl valid:   #123abc
+  hcl invalid: #123abz
+  hcl invalid: 123abc
+
+  ecl valid:   brn
+  ecl invalid: wat
+
+  pid valid:   000000001
+  pid invalid: 0123456789
+  Here are some invalid passports:
+
+  eyr:1972 cid:100
+  hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+  iyr:2019
+  hcl:#602927 eyr:1967 hgt:170cm
+  ecl:grn pid:012533040 byr:1946
+
+  hcl:dab227 iyr:2012
+  ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+  hgt:59cm ecl:zzz
+  eyr:2038 hcl:74454a iyr:2023
+  pid:3556412378 byr:2007
+  Here are some valid passports:
+
+  pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+  hcl:#623a2f
+
+  eyr:2029 ecl:blu cid:129 byr:1989
+  iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+  hcl:#888785
+  hgt:164cm byr:2001 iyr:2015 cid:88
+  pid:545766238 ecl:hzl
+  eyr:2022
+
+  iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+  Count the number of valid passports - those that have all required fields and valid values. Continue to treat cid as optional. In your batch file, how many passports are valid?
   """
 
   @doc """
@@ -52,10 +117,7 @@ defmodule Advent.Day04 do
   def part_1(input) do
     input
     |> parse()
-    |> Enum.count(fn passport ->
-      ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-      |> Enum.all?(&Map.has_key?(passport, &1))
-    end)
+    |> Enum.count(&all_keys?/1)
   end
 
   @doc """
@@ -65,6 +127,63 @@ defmodule Advent.Day04 do
   def part_2(input) do
     input
     |> parse()
+    |> Enum.count(&valid?/1)
+  end
+
+  defp valid?(passport) do
+    all_keys?(passport) and passport |> Map.keys() |> Enum.all?(&valid_field?(&1, Map.fetch!(passport, &1)))
+  end
+
+  # byr (Birth Year) - four digits; at least 1920 and at most 2002.
+  defp valid_field?("byr", value) do
+    value =~ ~r/^\d{4}$/ and String.to_integer(value) in 1920..2002
+  end
+
+  # iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+  defp valid_field?("iyr", value) do
+    value =~ ~r/^\d{4}$/ and String.to_integer(value) in 2010..2020
+  end
+
+  # eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+  defp valid_field?("eyr", value) do
+    value =~ ~r/^\d{4}$/ and String.to_integer(value) in 2020..2030
+  end
+
+  # hgt (Height) - a number followed by either cm or in:
+  #  - If cm, the number must be at least 150 and at most 193.
+  #  - If in, the number must be at least 59 and at most 76.
+  defp valid_field?("hgt", value) do
+    Regex.run(~r/^(\d+)(cm|in)$/, value, capture: :all_but_first)
+    |> case do
+      [num, "cm"] -> String.to_integer(num) in 150..193
+      [num, "in"] -> String.to_integer(num) in 59..76
+      _ -> false
+    end
+  end
+
+  # hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+  defp valid_field?("hcl", value) do
+    String.match?(value, ~r/^#[0-9a-f]{6}$/)
+  end
+
+  # ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+  defp valid_field?("ecl", value) do
+    value in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+  end
+
+  # pid (Passport ID) - a nine-digit number, including leading zeroes.
+  defp valid_field?("pid", value) do
+    String.match?(value, ~r/^[0-9]{9}$/)
+  end
+
+  # cid (Country ID) - ignored, missing or not.
+  defp valid_field?("cid", _value) do
+    true
+  end
+
+  defp all_keys?(passport) do
+    ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+    |> Enum.all?(&Map.has_key?(passport, &1))
   end
 
   defp parse(input) do
