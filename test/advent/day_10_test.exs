2e95a1296759c528c3142986ab62a94cc9babfca
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2020-12-10 14:37:44 +0100

    D10P2

--- test/advent/day_10_test.exs
+++ test/advent/day_10_test.exs
@@ -68,19 +68,66 @@ defmodule Advent.Day10Test do
   end
 
   describe "part 2" do
-    @tag :skip
-    test "example" do
+    test "example 1" do
       input = """
+      16
+      10
+      15
+      5
+      1
+      11
+      7
+      19
+      6
+      12
+      4
       """
 
-      assert Day10.part_2(input) == :foo
+      assert Day10.part_2(input) == 8
     end
 
-    @tag :skip
+    test "example 2" do
+      input = """
+      28
+      33
+      18
+      42
+      31
+      14
+      46
+      20
+      48
+      47
+      24
+      23
+      49
+      45
+      19
+      38
+      39
+      11
+      1
+      32
+      25
+      35
+      8
+      17
+      7
+      9
+      4
+      2
+      34
+      10
+      3
+      """
+
+      assert Day10.part_2(input) == 19_208
+    end
+
     test "puzzle input" do
       assert "puzzle_inputs/day_10.txt"
              |> File.read!()
-             |> Day10.part_2() == :foo
+             |> Day10.part_2() == 1_511_207_993_344
     end
   end
 end
