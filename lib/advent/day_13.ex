2f2c9354e4e07c4e122bf69c261591cfa1f83ddc
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2020-12-13 23:31:59 +0100

    D13P2

--- lib/advent/day_13.ex
+++ lib/advent/day_13.ex
@@ -26,9 +26,49 @@ defmodule Advent.Day13 do
   """
   @spec part_2(String.t()) :: integer
   def part_2(input) do
-    input
-    |> parse()
+    %{bus_ids: bus_ids} = parse(input)
+
+    bus_ids
+    |> Enum.with_index()
+    |> Enum.reject(fn {bus_id, _index} -> bus_id == :x end)
+    |> Enum.map(fn {bus_id, index} ->
+      # E.g. a bus with id 41 and index 7 will yield
+      #   41 ≡ 34 (mod N)
+      #   where N is the timestamp we're looking for
+      # This is the same as "On the timestamp of the first bus, bus 41 departed 34 minutes ago (and will depart in 7)"
+      # Now we just have to find the first N that satisfy all the bus lines
+      remainder = rem(rem(bus_id - index, bus_id) + bus_id, bus_id)
+      {bus_id, {remainder, bus_id}}
+    end)
+    |> Enum.reduce(&merge/2)
+    |> (fn {_bus_id, {time, _inc}} -> time end).()
   end
+
+  # Merge two bus lines into a single (new) bus line that is satisfied only when both the two inputs are
+  defp merge({bus_id_1, {time_1, inc_1}}, {bus_id_2, {time_2, inc_2}}) do
+    cond do
+      # We're done searching. We found the first time where both bus lines depart
+      # The new bus line will have id equal to least common multiple of the two and a modulo remainder equal to the
+      # found time
+      time_1 == time_2 ->
+        merged_bus_id = lcm(bus_id_1, bus_id_2)
+        {merged_bus_id, {time_1, merged_bus_id}}
+
+      # Timestamp 1 is too low. Increment it so that it is at least timestamp 2 and repeat
+      time_1 < time_2 ->
+        diff = time_2 - time_1
+        new_time = (div(diff - 1, inc_1) + 1) * inc_1 + time_1
+        merge({bus_id_1, {new_time, inc_1}}, {bus_id_2, {time_2, inc_2}})
+
+      # Timestamp 2 is too low. Increment it so that it is at least timestamp 1 and repeat
+      time_1 > time_2 ->
+        diff = time_1 - time_2
+        new_time = (div(diff - 1, inc_2) + 1) * inc_2 + time_2
+        merge({bus_id_1, {time_1, inc_1}}, {bus_id_2, {new_time, inc_2}})
+    end
+  end
+
+  def lcm(a, b), do: div(abs(a * b), Integer.gcd(a, b))
 
   defp parse(input) do
     [depart_time, bus_ids] =
--- test/advent/day_13_test.exs
+++ test/advent/day_13_test.exs
@@ -21,19 +21,39 @@ defmodule Advent.Day13Test do
   end
 
   describe "part 2" do
-    @tag :skip
-    test "example" do
+    test "example 1" do
       input = """
+      939
+      7,13,x,x,59,x,31,19
       """
 
-      assert Day13.part_2(input) == :foo
+      assert Day13.part_2(input) == 1_068_781
     end
 
-    @tag :skip
+    test "example 2" do
+      assert Day13.part_2("0\n17,x,13,19") == 3417
+    end
+
+    test "example 3" do
+      assert Day13.part_2("0\n67,7,59,61") == 754_018
+    end
+
+    test "example 4" do
+      assert Day13.part_2("0\n67,x,7,59,61") == 779_210
+    end
+
+    test "example 5" do
+      assert Day13.part_2("0\n67,7,x,59,61") == 1_261_476
+    end
+
+    test "example 6" do
+      assert Day13.part_2("0\n1789,37,47,1889") == 1_202_161_486
+    end
+
     test "puzzle input" do
       assert "puzzle_inputs/day_13.txt"
              |> File.read!()
-             |> Day13.part_2() == :foo
+             |> Day13.part_2() == 626_670_513_163_231
     end
   end
 end
