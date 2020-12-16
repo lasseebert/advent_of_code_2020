ada9e82aee8d39d102543904ca2f47a48d1fd910
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2020-12-16 12:07:52 +0100

    D16P2

--- lib/advent/day_16.ex
+++ lib/advent/day_16.ex
@@ -9,7 +9,7 @@ defmodule Advent.Day16 do
   @spec part_1(String.t()) :: integer
   def part_1(input) do
     data = parse(input)
-    valid_ranges = data.rules |> Map.values() |> List.flatten()
+    valid_ranges = data.rules |> Enum.flat_map(&elem(&1, 1))
 
     data.tickets
     |> List.flatten()
@@ -20,10 +20,53 @@ defmodule Advent.Day16 do
   @doc """
   Part 2
   """
-  @spec part_2(String.t()) :: integer
+  @spec part_2(String.t()) :: map
   def part_2(input) do
-    input
-    |> parse()
+    data = parse(input)
+    valid_ranges = data.rules |> Enum.flat_map(&elem(&1, 1))
+
+    tickets =
+      data.tickets
+      |> Enum.reject(fn ticket ->
+        Enum.any?(ticket, fn value ->
+          Enum.all?(valid_ranges, &(not (value in &1)))
+        end)
+      end)
+
+    tickets_data =
+      [data.own_ticket | tickets]
+      |> Enum.zip()
+      |> Enum.map(&Tuple.to_list/1)
+      |> Enum.with_index()
+
+    matching_indexes = find_rules_index(data.rules, tickets_data, %{})
+
+    data.own_ticket
+    |> Enum.with_index()
+    |> Enum.into(%{}, fn {value, index} -> {Map.fetch!(matching_indexes, index), value} end)
+  end
+
+  defp find_rules_index([], _tickets_data, acc), do: acc
+
+  defp find_rules_index([{name, ranges} = rule | rules], tickets_data, acc) do
+    tickets_data
+    |> Enum.filter(fn {values, _index} ->
+      Enum.all?(values, fn value ->
+        Enum.any?(ranges, fn range -> value in range end)
+      end)
+    end)
+    |> Enum.map(&elem(&1, 1))
+    |> case do
+      [index] ->
+        tickets_data = tickets_data |> Enum.reject(fn {_values, value_index} -> value_index == index end)
+        acc = Map.put(acc, index, name)
+        find_rules_index(rules, tickets_data, acc)
+
+      [_, _ | _] ->
+        # Put last in queue and wait for a unique match
+        rules = rules ++ [rule]
+        find_rules_index(rules, tickets_data, acc)
+    end
   end
 
   defp parse(input) do
@@ -35,7 +78,7 @@ defmodule Advent.Day16 do
 
     own_ticket = parse_ticket(own_ticket)
     tickets = Enum.map(tickets, &parse_ticket/1)
-    rules = Enum.into(rules, %{}, &parse_rule/1)
+    rules = Enum.map(rules, &parse_rule/1)
 
     %{
       rules: rules,
--- test/advent/day_16_test.exs
+++ test/advent/day_16_test.exs
@@ -31,19 +31,35 @@ defmodule Advent.Day16Test do
   end
 
   describe "part 2" do
-    @tag :skip
     test "example" do
       input = """
+      class: 0-1 or 4-19
+      row: 0-5 or 8-19
+      seat: 0-13 or 16-19
+
+      your ticket:
+      11,12,13
+
+      nearby tickets:
+      3,9,18
+      15,1,5
+      5,14,9
       """
 
-      assert Day16.part_2(input) == :foo
+      assert Day16.part_2(input) == %{
+               "row" => 11,
+               "class" => 12,
+               "seat" => 13
+             }
     end
 
-    @tag :skip
     test "puzzle input" do
       assert "puzzle_inputs/day_16.txt"
              |> File.read!()
-             |> Day16.part_2() == :foo
+             |> Day16.part_2()
+             |> Enum.filter(fn {name, _value} -> String.starts_with?(name, "departure") end)
+             |> Enum.map(&elem(&1, 1))
+             |> Enum.reduce(fn a, b -> a * b end) == 4_381_476_149_273
     end
   end
 end
