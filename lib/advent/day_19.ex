5978c65ddaceb13f88057833098bf300fb31ee8c
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2020-12-20 00:02:14 +0100

    D19P2

--- lib/advent/day_19.ex
+++ lib/advent/day_19.ex
@@ -13,49 +13,46 @@ defmodule Advent.Day19 do
     Enum.count(data.messages, &valid_string?(&1, data.grammar))
   end
 
-  defp valid_string?(string, grammar) do
-    case match_rule(0, string, grammar) do
-      {:ok, ""} -> true
-      {:ok, _} -> false
-      :error -> false
-    end
-  end
-
-  defp match_rule(num, string, grammar) do
-    grammar
-    |> Map.fetch!(num)
-    |> Enum.reduce_while(:error, fn expression, :error ->
-      case match_expression(expression, string, grammar) do
-        {:ok, rest} -> {:halt, {:ok, rest}}
-        :error -> {:cont, :error}
-      end
-    end)
-  end
-
-  defp match_expression([{:literal, literal_char}], string, _grammar) do
-    if String.starts_with?(string, literal_char) do
-      {:ok, String.slice(string, 1..-1)}
-    else
-      :error
-    end
-  end
-
-  defp match_expression(rules, string, grammar) do
-    Enum.reduce_while(rules, {:ok, string}, fn {:rule, rule_num}, {:ok, string} ->
-      case match_rule(rule_num, string, grammar) do
-        {:ok, rest} -> {:cont, {:ok, rest}}
-        :error -> {:halt, :error}
-      end
-    end)
-  end
-
   @doc """
   Part 2
   """
   @spec part_2(String.t()) :: integer
   def part_2(input) do
-    input
-    |> parse()
+    %{grammar: grammar, messages: messages} = parse(input)
+
+    grammar =
+      grammar
+      |> Map.put(8, [[rule: 42], [rule: 42, rule: 8]])
+      |> Map.put(11, [[rule: 42, rule: 31], [rule: 42, rule: 11, rule: 31]])
+
+    Enum.count(messages, &valid_string?(&1, grammar))
+  end
+
+  defp valid_string?(string, grammar) do
+    results = match_rule(0, string, grammar)
+    {:ok, ""} in results
+  end
+
+  defp match_rule(num, string, grammar, indent \\ 0) do
+    grammar
+    |> Map.fetch!(num)
+    |> Enum.flat_map(&match_expression(&1, string, grammar, indent + 1))
+  end
+
+  defp match_expression([{:literal, literal_char}], string, _grammar, _indent) do
+    if String.starts_with?(string, literal_char) do
+      [ok: String.slice(string, 1..-1)]
+    else
+      []
+    end
+  end
+
+  defp match_expression([], string, _grammar, _indent), do: [{:ok, string}]
+
+  defp match_expression([{:rule, rule_num} | rules], string, grammar, indent) do
+    rule_num
+    |> match_rule(string, grammar, indent)
+    |> Enum.flat_map(fn {:ok, string} -> match_expression(rules, string, grammar, indent) end)
   end
 
   defp parse(input) do
--- test/advent/day_19_test.exs
+++ test/advent/day_19_test.exs
@@ -26,24 +26,126 @@ defmodule Advent.Day19Test do
     test "puzzle input" do
       assert "puzzle_inputs/day_19.txt"
              |> File.read!()
-             |> Day19.part_1() == :foo
+             |> Day19.part_1() == 213
     end
   end
 
   describe "part 2" do
-    @tag :skip
-    test "example" do
-      input = """
+    defp example_input(messages) do
+      grammar = """
+      42: 9 14 | 10 1
+      9: 14 27 | 1 26
+      10: 23 14 | 28 1
+      1: "a"
+      11: 42 31
+      5: 1 14 | 15 1
+      19: 14 1 | 14 14
+      12: 24 14 | 19 1
+      16: 15 1 | 14 14
+      31: 14 17 | 1 13
+      6: 14 14 | 1 14
+      2: 1 24 | 14 4
+      0: 8 11
+      13: 14 3 | 1 12
+      15: 1 | 14
+      17: 14 2 | 1 7
+      23: 25 1 | 22 14
+      28: 16 1
+      4: 1 1
+      20: 14 14 | 1 15
+      3: 5 14 | 16 1
+      27: 1 6 | 14 18
+      14: "b"
+      21: 14 1 | 1 14
+      25: 1 1 | 1 14
+      22: 14 14
+      8: 42
+      26: 14 22 | 1 20
+      18: 15 15
+      7: 14 5 | 1 21
+      24: 14 1
       """
 
-      assert Day19.part_2(input) == :foo
+      "#{grammar}\n#{messages}"
     end
 
-    @tag :skip
+    test "example" do
+      input =
+        """
+        abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+        bbabbbbaabaabba
+        babbbbaabbbbbabbbbbbaabaaabaaa
+        aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+        bbbbbbbaaaabbbbaaabbabaaa
+        bbbababbbbaaaaaaaabbababaaababaabab
+        ababaaaaaabaaab
+        ababaaaaabbbaba
+        baabbaaaabbaaaababbaababb
+        abbbbabbbbaaaababbbbbbaaaababb
+        aaaaabbaabaaaaababaa
+        aaaabbaaaabbaaa
+        aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+        babaaabbbaaabaababbaabababaaab
+        aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+        """
+        |> example_input()
+
+      assert Day19.part_2(input) == 12
+    end
+
+    @valid """
+           bbabbbbaabaabba
+           babbbbaabbbbbabbbbbbaabaaabaaa
+           aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+           bbbbbbbaaaabbbbaaabbabaaa
+           bbbababbbbaaaaaaaabbababaaababaabab
+           ababaaaaaabaaab
+           ababaaaaabbbaba
+           baabbaaaabbaaaababbaababb
+           abbbbabbbbaaaababbbbbbaaaababb
+           aaaaabbaabaaaaababaa
+           aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+           aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+           """
+           |> String.split("\n", trim: true)
+
+    """
+    abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+    bbabbbbaabaabba
+    babbbbaabbbbbabbbbbbaabaaabaaa
+    aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+    bbbbbbbaaaabbbbaaabbabaaa
+    bbbababbbbaaaaaaaabbababaaababaabab
+    ababaaaaaabaaab
+    ababaaaaabbbaba
+    baabbaaaabbaaaababbaababb
+    abbbbabbbbaaaababbbbbbaaaababb
+    aaaaabbaabaaaaababaa
+    aaaabbaaaabbaaa
+    aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+    babaaabbbaaabaababbaabababaaab
+    aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+    """
+    |> String.split("\n", trim: true)
+    |> Enum.each(fn message ->
+      expected =
+        if message in @valid do
+          1
+        else
+          0
+        end
+
+      if not String.starts_with?(message, "#") do
+        test "part example #{message}" do
+          assert unquote(message) |> example_input() |> Day19.part_2() == unquote(expected)
+        end
+      end
+    end)
+
     test "puzzle input" do
       assert "puzzle_inputs/day_19.txt"
              |> File.read!()
-             |> Day19.part_2() == :foo
+             |> Day19.part_2() == 325
     end
   end
 end
