57908db7c2e00ef713e3bb2e5c84bd3d88d06d30
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2020-12-21 11:29:33 +0100

    D21P2

--- lib/advent/day_21.ex
+++ lib/advent/day_21.ex
@@ -9,31 +9,34 @@ defmodule Advent.Day21 do
   @spec part_1(String.t()) :: integer
   def part_1(input) do
     list = parse(input)
+    safe = safe_ingredients(list)
 
+    ingredients_with_dups = Enum.flat_map(list, &elem(&1, 0))
+
+    safe
+    |> Enum.map(fn ingredient -> Enum.count(ingredients_with_dups, &(&1 == ingredient)) end)
+    |> Enum.sum()
+  end
+
+  defp safe_ingredients(food_list) do
     allergen_to_foods =
-      list
+      food_list
       |> Enum.flat_map(fn {food, allergens} -> Enum.map(allergens, &{&1, food}) end)
       |> Enum.group_by(&elem(&1, 0), &elem(&1, 1))
 
     with_allergens =
-      list
+      food_list
       |> Enum.flat_map(fn {food, allergens} ->
         Enum.filter(food, fn ingredient ->
           Enum.any?(allergens, fn allergen ->
-            allergen_to_foods
-            |> Map.fetch!(allergen)
-            |> Enum.all?(&(ingredient in &1))
+            allergen_to_foods |> Map.fetch!(allergen) |> Enum.all?(&(ingredient in &1))
           end)
         end)
       end)
       |> Enum.uniq()
 
-    ingredients_with_dups = Enum.flat_map(list, &elem(&1, 0))
-    ingredients_without_allergens = Enum.uniq(ingredients_with_dups) -- with_allergens
-
-    ingredients_without_allergens
-    |> Enum.map(fn ingredient -> Enum.count(ingredients_with_dups, &(&1 == ingredient)) end)
-    |> Enum.sum()
+    ingredients = food_list |> Enum.flat_map(&elem(&1, 0)) |> Enum.uniq()
+    ingredients -- with_allergens
   end
 
   @doc """
@@ -41,8 +44,57 @@ defmodule Advent.Day21 do
   """
   @spec part_2(String.t()) :: integer
   def part_2(input) do
-    input
-    |> parse()
+    list = parse(input)
+    safe = safe_ingredients(list)
+
+    list =
+      Enum.map(list, fn {food, allergen} ->
+        {food -- safe, allergen}
+      end)
+
+    all_ingredients = list |> Enum.flat_map(&elem(&1, 0)) |> Enum.uniq()
+    all_allergens = list |> Enum.flat_map(&elem(&1, 1)) |> Enum.uniq()
+
+    true = length(all_ingredients) == length(all_allergens)
+
+    candidates =
+      Enum.reduce(list, %{}, fn {food, allergens}, candidates ->
+        Enum.reduce(food, candidates, fn ingredient, candidates ->
+          Enum.reduce(allergens, candidates, fn allergen, candidates ->
+            Map.update(candidates, ingredient, MapSet.new([allergen]), &MapSet.put(&1, allergen))
+          end)
+        end)
+      end)
+
+    [mapping] = match(all_ingredients, all_allergens, %{}, list, candidates)
+
+    mapping
+    |> Enum.sort_by(&elem(&1, 1))
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.join(",")
+  end
+
+  defp match([], [], acc, list, _candidates) do
+    list
+    |> Enum.all?(fn {food, allergens} ->
+      mapped_food = Enum.map(food, &Map.fetch!(acc, &1))
+      Enum.all?(allergens, &(&1 in mapped_food))
+    end)
+    |> case do
+      true -> [acc]
+      false -> []
+    end
+  end
+
+  defp match([ingredient | ingredients], allergens, acc, list, candidates) do
+    candidates
+    |> Map.fetch!(ingredient)
+    |> Enum.filter(&(&1 in allergens))
+    |> Enum.flat_map(fn allergen ->
+      allergens = allergens -- [allergen]
+      acc = Map.put(acc, ingredient, allergen)
+      match(ingredients, allergens, acc, list, candidates)
+    end)
   end
 
   defp parse(input) do
--- test/advent/day_21_test.exs
+++ test/advent/day_21_test.exs
@@ -36,19 +36,47 @@ defmodule Advent.Day21Test do
   end
 
   describe "part 2" do
-    @tag :skip
     test "example" do
       input = """
+      mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+      trh fvjkl sbzzf mxmxvkd (contains dairy)
+      sqjhc fvjkl (contains soy)
+      sqjhc mxmxvkd sbzzf (contains fish)
       """
 
-      assert Day21.part_2(input) == :foo
+      assert Day21.part_2(input) == "mxmxvkd,sqjhc,fvjkl"
     end
 
-    @tag :skip
+    test "translated example" do
+      input = """
+      a b c d (contains dairy, fish)
+      e f g a (contains dairy)
+      c f (contains soy)
+      c a g (contains fish)
+      """
+
+      # a: dairy
+      # c: fish
+      # f: soy
+
+      assert Day21.part_2(input) == "a,c,f"
+    end
+
+    test "translated example simplified" do
+      input = """
+      a c (contains dairy, fish)
+      f a (contains dairy)
+      c f (contains soy)
+      c a (contains fish)
+      """
+
+      assert Day21.part_2(input) == "a,c,f"
+    end
+
     test "puzzle input" do
       assert "puzzle_inputs/day_21.txt"
              |> File.read!()
-             |> Day21.part_2() == :foo
+             |> Day21.part_2() == "fsr,skrxt,lqbcg,mgbv,dvjrrkv,ndnlm,xcljh,zbhp"
     end
   end
 end
